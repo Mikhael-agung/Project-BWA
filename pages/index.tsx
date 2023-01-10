a7f7cce1490d8ddcc385b65f6478dcad2efcef3f
mikhaelagung <mikhaelagung3@gmail.com>
2023-01-10 21:38:06 +0700

    menambahkan file baru

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,6 @@ import {useEffect} from 'react';
 import AOS from 'aos';
 import Navbar from '../components/orgnisms/navbar';
 import MainBanner from '../components/orgnisms/MainBanner';
-import Transaction from '../components/orgnisms/Transaction';
 import TransactionStep from '../components/orgnisms/Transaction';
 import FeaturedGame from '../components/orgnisms/FeatureGames';
 import Reached from '../components/orgnisms/Reached';
