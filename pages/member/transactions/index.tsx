0fd264760920fdab1db5904d64cdbd4c74c809d0
mikhaelagung <mikhaelagung3@gmail.com>
2023-01-16 00:16:40 +0700

    modifikasi file

--- pages/member/transactions/index.tsx
+++ pages/member/transactions/index.tsx
@@ -12,7 +12,7 @@ export default function transaction() {
           <h2 className="text-4xl fw-bold color-palette-1 mb-30">My Transactions</h2>
           <div className="mb-30">
             <p className="text-lg color-palette-2 mb-12">You’ve spent</p>
-            <h3 className="text-5xl fw-medium color-palette-1">Rp 4.518.000.500</h3>
+            <h3 className="text-5xl fw-medium color-palette-1">Rp 4.518.000</h3>
           </div>
           <div className="row mt-30 mb-20">
             <div className="col-lg-12 col-12 main-content">
